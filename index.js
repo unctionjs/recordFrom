054ea00075956a33ce23d7efd73eefcf06115277
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-27 19:46:29 -0700

    We have a new, more formal, name for iterables

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import {reverse} from "ramda"
 export default function recordFrom (keychain: KeyChainType): Function {
   const reverseKeychain = reverse(keychain)
 
-  return function recordFromKeychain (value: any): IterableType {
+  return function recordFromKeychain (value: any): FunctorType {
     return reduceValues(
       (accumulated: RecordType): Function => (key: KeyType): RecordType => attach(key)(accumulated)({})
     )(
